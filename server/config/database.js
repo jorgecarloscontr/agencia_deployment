300d0fe130d6adf8c44f7a019e037c03e5447b71
Jorge Carlos Alvarado Contreras <jorge_carloscontr@hotmail.com>
2020-03-02 01:18:15 -0600

    ajuste de conexion a bd

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config({path: 'variables.env'});
 
-module.exports = new Sequelize(process.env.BD_NOMRE,process.env.BD_USER,process.env.BD_PASS,{
+module.exports = new Sequelize(process.env.BD_NOMBRE,process.env.BD_USER,process.env.BD_PASS,{
     host: process.env.BD_HOST,
     port: process.env.BD_PORT,
     dialect: 'mysql',
